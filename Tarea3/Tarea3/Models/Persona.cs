a9382934789b6da4175fd152d7d4f7f1be50dc33
josefgonzalezz <jgonzalez50082@ufide.ac.cr>
2026-03-30 23:20:43 -0600

    Modelo persona y nugets
    Modelo persona

---
+++ Tarea3/Tarea3/Models/Persona.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace Tarea3.Models
+{
+    public class Persona : IdentityUser<int>
+    {
+        [Required(ErrorMessage = "La cédula es obligatoria")]
+        public string Cedula { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; }
+
+        [Range(1, 120, ErrorMessage = "Edad inválida")]
+        public int Edad { get; set; }
+
+    }
+}
