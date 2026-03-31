6fca501dcc72d1120f81ed29ac221b48148332ad
josefgonzalezz <jgonzalez50082@ufide.ac.cr>
2026-03-30 23:40:22 -0600

    Compra repository
    Compra

---
+++ Tarea3/Tarea3/Repositories/CompraRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tarea3.Models;
+using Tarea3.Data;
+
+namespace Tarea3.Repositories
+{
+    public class CompraRepository : ICompraRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CompraRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool Crear(Compra compra)
+        {
+            _context.Compras.Add(compra);
+            _context.SaveChanges();
+            return true;
+        }
+
+        public List<Compra> ObtenerPorCliente(string nombreCliente)
+        {
+            return _context.Compras
+                .Include(c => c.Evento)
+                .Where(c => c.NombreCliente == nombreCliente)
+                .ToList();
+        }
+    }
+}
---
+++ Tarea3/Tarea3/Repositories/ICompraRepository.cs
@@ -0,0 +1,10 @@
+﻿using Tarea3.Models;
+
+namespace Tarea3.Repositories
+{
+    public interface ICompraRepository
+    {
+        List<Compra> ObtenerPorCliente(string nombreCliente);
+        bool Crear(Compra compra);
+    }
+}
