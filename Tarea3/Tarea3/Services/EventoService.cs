3228c027e4e7b8c97a42edafc0674335218b79d1
DavidSanabriaV <babitosanabria@gmail.com>
2026-03-30 23:45:56 -0600

    eventoservice
    creacion del service para los eventos

---
+++ Tarea3/Tarea3/Services/EventoService.cs
@@ -0,0 +1,21 @@
+﻿using Tarea3.Models;
+using Tarea3.Repositories;
+
+namespace Tarea3.Services
+{
+    public class EventoService : IEventoService
+    {
+        private readonly IEventoRepository _repo;
+
+        public EventoService(IEventoRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public List<Evento> ObtenerTodos() => _repo.ObtenerTodos();
+
+        public Evento? ObtenerPorId(int id) => _repo.ObtenerPorId(id);
+
+        public bool Crear(Evento evento) => _repo.Crear(evento);
+    }
+}
--- Tarea3/Tarea3/Services/IEventoService.cs
+++ Tarea3/Tarea3/Services/IEventoService.cs
@@ -1,6 +1,11 @@
-﻿namespace Tarea3.Services
+﻿using Tarea3.Models;
+
+namespace Tarea3.Services
 {
     public interface IEventoService
     {
+        List<Evento> ObtenerTodos();
+        Evento? ObtenerPorId(int id);
+        bool Crear(Evento evento);
     }
 }
