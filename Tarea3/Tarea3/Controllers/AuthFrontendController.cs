2dc6fcc5af2db4730adfa7da6460cecbe2591319
josefgonzalezz <jgonzalez50082@ufide.ac.cr>
2026-03-31 00:26:50 -0600

    Cambios
    cambios

---
+++ Tarea3/Tarea3/Controllers/AuthFrontendController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tarea3.Controllers
+{
+    [Route("auth-frontend")]
+    public class AuthFrontendController : Controller
+    {
+        [HttpGet("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet("register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ Tarea3/Tarea3/Controllers/CompraFrontendController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tarea3.Controllers
+{
+    [Route("compras")]
+    [Authorize]
+    public class ComprasFrontendController : Controller
+    {
+        [HttpGet("comprar/{id}")]
+        public IActionResult Comprar(int id) => View();
+
+        [HttpGet("historial")]
+        public IActionResult Historial() => View();
+    }
+}
