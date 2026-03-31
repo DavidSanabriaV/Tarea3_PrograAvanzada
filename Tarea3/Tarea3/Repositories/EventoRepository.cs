47ec3999e1cd36767373018c2accbb32b947b985
DavidSanabriaV <babitosanabria@gmail.com>
2026-03-30 23:36:51 -0600

    Eventos repository
    codigo completo del repository de eventos

---
+++ Tarea3/Tarea3/Repositories/EventoRepository.cs
@@ -0,0 +1,32 @@
+﻿using Tarea3.Data;
+using Tarea3.Models;
+
+namespace Tarea3.Repositories
+{
+    public class EventoRepository : IEventoRepository
+    {
+        private readonly AppDbContext _context;
+
+        public EventoRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Evento> ObtenerTodos()
+        {
+            return _context.Eventos.ToList();
+        }
+
+        public Evento? ObtenerPorId(int id)
+        {
+            return _context.Eventos.FirstOrDefault(e => e.Id == id);
+        }
+
+        public bool Crear(Evento evento)
+        {
+            _context.Eventos.Add(evento);
+            _context.SaveChanges();
+            return true;
+        }
+    }
+}
--- Tarea3/Tarea3/Repositories/IEventoRepository.cs
+++ Tarea3/Tarea3/Repositories/IEventoRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace Tarea3.Repositories
+﻿using Tarea3.Models;
+
+namespace Tarea3.Repositories
 {
     public interface IEventoRepository
     {
+        List<Evento> ObtenerTodos();
+        Evento? ObtenerPorId(int id);
+        bool Crear(Evento evento);
     }
 }
