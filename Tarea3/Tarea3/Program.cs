42a096dc312f3f6a0e145d1be6f41464168b3010
DavidSanabriaV <babitosanabria@gmail.com>
2026-03-31 00:53:19 -0600

    Program
    codigo del program para la funcionalidad del proyecto

--- Tarea3/Tarea3/Program.cs
+++ Tarea3/Tarea3/Program.cs
@@ -1,25 +1,73 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Tarea3.Constants;
+using Tarea3.Data;
+using Tarea3.Models;
+using Tarea3.Repositories;
+using Tarea3.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+builder.Services.AddIdentity<Persona, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>();
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/auth/login";
+    options.AccessDeniedPath = "/auth/login";
+});
+
+// Repositories
+builder.Services.AddScoped<IEventoRepository, EventoRepository>();
+builder.Services.AddScoped<ICompraRepository, CompraRepository>();
+
+// Services
+builder.Services.AddScoped<IEventoService, EventoService>();
+builder.Services.AddScoped<ICompraService, CompraService>();
+builder.Services.AddScoped<IAccountService, AccountService>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+// Seed roles y admin
+using (var scope = app.Services.CreateScope())
 {
-    app.UseExceptionHandler("/Home/Error");
-}
-app.UseRouting();
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<Persona>>();
 
+    foreach (var role in new[] { Roles.Admin, Roles.User })
+    {
+        if (!await roleManager.RoleExistsAsync(role))
+            await roleManager.CreateAsync(new IdentityRole(role));
+    }
+
+    var adminEmail = "admin@tiquetes.com";
+    if (await userManager.FindByEmailAsync(adminEmail) == null)
+    {
+        var admin = new Persona
+        {
+            UserName = adminEmail,
+            Email = adminEmail,
+            Nombre = "Administrador",
+            Cedula = "000000000",
+            Edad = 30
+        };
+        await userManager.CreateAsync(admin, "Admin123!");
+        await userManager.AddToRoleAsync(admin, Roles.Admin);
+    }
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+app.MapControllerRoute(name: "default", pattern: "{controller=Auth}/{action=Login}/{id?}");
 
 app.Run();
